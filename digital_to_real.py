35138b9816b6708afb19d0053a8eefe6379a7eb0
EshanSankar <eshansankar12@gmail.com>
2025-08-06 17:16:35 -0400

    todo: more safety checking and fixes

--- digital_to_real.py
+++ digital_to_real.py
@@ -69,7 +69,6 @@ OT2_COORDS = {
 # [-0.08,0.2] #38 cm			[-0.19, 0.18] #42 cm					[-0.1, 0]					  [-0.1, 0]				
 # pipette dims: 80x130
 
-
 class Assets():
 	def __init__(self, asset_path):
 		self.assets = {}
@@ -79,15 +78,12 @@ class Assets():
 				print(asset)
 				#reactor_plate wash_station tip_rack electrode_tip_rack solution_rack
 
-
-				#add_reference_to_stage(usd_path=f"{asset_path}/{asset}.usd", prim_path=f"/World/{asset}")
-				#self.assets[f"{asset}"] = [asset["slot"], SingleXFormPrim(prim_path=f"/World/{asset}", name=f"{asset}")]
-				#self.assets[f"{asset}"][1].set_world_pose(position=np.array([[
-				#	OT2_COORDS[asset["slot"]][0] + OT2_SLOT_DIMS["x"],
-				#	OT2_COORDS[asset["slot"]][1] + OT2_SLOT_DIMS["y"],
-				#	OT2_BASE_HEIGHT]]) / get_stage_units())
-
-
+				add_reference_to_stage(usd_path=f"{asset_path}/{asset}.usd", prim_path=f"/World/{asset}")
+				self.assets[f"{asset}"] = [asset["slot"], SingleXFormPrim(prim_path=f"/World/{asset}", name=f"{asset}")]
+				self.assets[f"{asset}"][1].set_world_pose(position=np.array([[
+					OT2_COORDS[asset["slot"]][0] + OT2_SLOT_DIMS["x"],
+					OT2_COORDS[asset["slot"]][1] + OT2_SLOT_DIMS["y"],
+					OT2_BASE_HEIGHT]]) / get_stage_units())
 
 class SimulatedWorld(Node):
 	def __init__(self):
@@ -96,19 +92,17 @@ class SimulatedWorld(Node):
 		self.ros_world = World(stage_units_in_meters=1.0)
 		self.ros_world.scene.add_default_ground_plane()
 		self.ot2_sub = self.create_subscription(JointState, "/sim_ot2/target_joint_states", self.ot2_cb, 10)
-		#self.ot2_joint_targets = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
-		self.ot2_joint_targets = np.array([0.0, 0.0])
+		self.ot2_joint_targets = np.array([0.0, 0.0, 0.0])
 		self.xarm_sub = self.create_subscription(JointState, "/sim_xarm/target_joint_states", self.xarm_cb, 10)
 		self.xarm_joint_targets = np.array([2.8653342723846436,-0.08610617369413376, -0.4979282319545746,4.3559088706970215, 1.3179675340652466, 1.0210156440734863, 0])
 		self.ot2_pub = self.create_publisher(Float32MultiArray, "/sim_ot2/joint_states", 10)
 		self.xarm_pub = self.create_publisher(Float32MultiArray, "/sim_xarm/joint_states", 10)
 		# /sim_robot/joint_states: [[joint1, ..., jointN], [joint1_goal, ..., jointN_goal]]
-		self.ot2_joints = ["PrismaticJointMiddleBar", "PrismaticJointPipetteHolder"]
-		self.ot2_joint_indices = np.array([0, 1])
-		self.xarm_joints = ["joint1", "joint2", "joint3", "joint4", "joint5", "joint6", "drive_joint"]
+		self.ot2_joints = ["PrismaticJointMiddleBar", "PrismaticJointPipetteHolder", "PrismaticJointLeftPipette"]
+		self.ot2_joint_indices = np.array([0, 1, 2])
+		self.xarm_joints = ["joint1", "joint2", "joint3", "joint4", "joint5", "joint6", "GRIPPER"] #TODO: figure this out
 		self.xarm_joint_indices = np.array([0, 1, 2, 3, 4, 5, 6])
-		#self.ot2_dim = [MultiArrayDimension(label="joints", size=2, stride=10), MultiArrayDimension(label="goals", size=5, stride=5)]
-		self.ot2_dim = [MultiArrayDimension(label="joints", size=2, stride=4), MultiArrayDimension(label="goals", size=2, stride=2)]
+		self.ot2_dim = [MultiArrayDimension(label="joints", size=2, stride=6), MultiArrayDimension(label="goals", size=3, stride=3)]
 		self.xarm_dim = [MultiArrayDimension(label="joints", size=2, stride=14), MultiArrayDimension(label="goals", size=7, stride=7)]
 		self.asset_dim = [MultiArrayDimension(label="pose", size=7, stride=7)]
 		self.safety = 1
@@ -120,21 +114,19 @@ class SimulatedWorld(Node):
 		self.asset_pubs = {}
 		self.asset_cmd_poses = {}
 		self.asset_initial_poses = {}
-		#for asset in self.assets:
-		#	self.asset_subs[f"{asset}"] = self.create_subscription(PoseStamped, f"/asset_position_{self.assets[{asset}][0]}", lambda msg: self.asset_cb(msg, f"{asset}"), 10)
-		#	self.asset_cmd_poses[f"{asset}"] = np.concatenate(self.assets[f"{asset}"][1].get_world_pose()[0], self.assets[f"{asset}"][1].get_world_pose()[1], axis=1)
-		#	self.asset_initial_poses[f"{asset}"] = self.asset_cmd_poses[f"{asset}"].copy()
-		#	self.asset_pubs[f"{asset}"] = self.create_publisher(Float32MultiArray, f"/sim_{asset}/pose", 10)
+		for asset in self.assets:
+			self.asset_subs[f"{asset}"] = self.create_subscription(PoseStamped, f"/asset_position_{self.assets[{asset}][0]}", lambda msg: self.asset_cb(msg, f"{asset}"), 10)
+			self.asset_cmd_poses[f"{asset}"] = np.concatenate(self.assets[f"{asset}"][1].get_world_pose()[0], self.assets[f"{asset}"][1].get_world_pose()[1], axis=1)
+			self.asset_initial_poses[f"{asset}"] = self.asset_cmd_poses[f"{asset}"].copy()
+			self.asset_pubs[f"{asset}"] = self.create_publisher(Float32MultiArray, f"/sim_{asset}/pose", 10)
 	def asset_cb(self, msg, asset):
 		self.asset_cmd_poses[asset][0], self.asset_cmd_poses[f"{asset}"][1], self.asset_cmd_poses[f"{asset}"][2] = msg.pose.position.x, msg.pose.position.y, msg.pose.position.z
 		self.asset_cmd_poses[asset][3], self.asset_cmd_poses[f"{asset}"][4], self.asset_cmd_poses[f"{asset}"][5], self.asset_cmd_poses[f"{asset}"][6] = msg.pose.orientation.w, msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z
 		self.asset_cmd_poses[asset] = np.substract(self.asset_cmd_poses[asset], self.asset_initial_poses[asset])
 	def ot2_cb(self, msg):
 		self.ot2_joint_targets = msg.position
-		self.ot2_reached_goal = False
 	def xarm_cb(self, msg):
 		self.xarm_joint_targets = np.array(msg.position)[:7] if msg.position[7] == 0.0 else np.add(np.array(msg.position)[:7], xarm.get_joint_positions()[0][:7])
-		self.xarm_reached_goal = False
 	def safety_cb(self, msg):
 		self.safety = int(msg.data)
 	def run_simulation(self):
@@ -156,12 +148,12 @@ class SimulatedWorld(Node):
 					ot2.apply_action(ArticulationAction(joint_positions=self.ot2_joint_targets, joint_indices=self.ot2_joint_indices))
 					xarm.set_joint_position_targets(positions=self.xarm_joint_targets, joint_indices=self.xarm_joint_indices)
 					print(f"Moved xArm: {self.xarm_joint_targets}")
-					self.ot2_pub.publish(Float32MultiArray(layout=MultiArrayLayout(dim=self.ot2_dim, data_offset=0), data=np.concatenate((ot2.get_joint_positions()[:2], self.ot2_joint_targets))))
+					self.ot2_pub.publish(Float32MultiArray(layout=MultiArrayLayout(dim=self.ot2_dim, data_offset=0), data=np.concatenate((ot2.get_joint_positions()[:3], self.ot2_joint_targets))))
 					self.xarm_pub.publish(Float32MultiArray(layout=MultiArrayLayout(dim=self.xarm_dim, data_offset=0), data=np.concatenate((xarm.get_joint_positions()[0][:7], self.xarm_joint_targets))))
 					# mirror asset positions from real-world
-					#for asset in self.assets:
-					#	self.assets[f"{asset}"][1].set_world_pose(position=self.asset_cmd_poses[f"{asset}"][:3], orientation=self.asset_cmd_poses[f"{asset}"][3:])
-					#	self.asset_pubs[f"{asset}"].publish(Float32MultiArray(layout=MultiArrayLayout(dim=self.asset_dim, data_offset=0), data=self.asset_cmd_poses[f"{asset}"]))
+					for asset in self.assets:
+						self.assets[f"{asset}"][1].set_world_pose(position=self.asset_cmd_poses[f"{asset}"][:3], orientation=self.asset_cmd_poses[f"{asset}"][3:])
+						self.asset_pubs[f"{asset}"].publish(Float32MultiArray(layout=MultiArrayLayout(dim=self.asset_dim, data_offset=0), data=self.asset_cmd_poses[f"{asset}"]))
 		self.timeline.stop()
 		self.destroy_node()
 		simulation_app.close()
--- safety_checker.py
+++ safety_checker.py
@@ -1,9 +1,10 @@
-#TODO: integrate with d2r and r2d files
+#TODO: write collision checker and handler for robots moving objecs
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray, Int8 # -1, 0, 1
+from std_msgs.msg import String, Float32MultiArray, Int8 # -1, 0, 1
 import numpy as np
+from digital_to_real import Assets
 
 class SafetyChecker(Node):
     def __init__(self):
@@ -13,25 +14,31 @@ class SafetyChecker(Node):
         self.ot2_sub = self.create_subscription(Float32MultiArray, "/sim_ot2/joint_states", self.ot2_cb, 10)
         self.xarm_joints = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
         self.xarm_targets = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-        # self.ot2_joints = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
-        # self.ot2_targets = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
-        self.ot2_joints = np.array([0.0, 0.0])
-        self.ot2_targets = np.array([0.0, 0.0])
+        self.ot2_joints = np.array([0.0, 0.0, 0.0])
+        self.ot2_targets = np.array([0.0, 0.0, 0.0])
+
+        # 0: continue current action, 1: safe, -1: unsafe
         self.status_pub = self.create_publisher(Int8, "/safety_checker/status_int", 10)
-        self.status = Int8(data=1) # 0: continue current action, 1: safe, -1: unsafe
+        self.target_asset_sub = self.create_subscription(String, "/safety_checker/target_asset", self.target_asset_cb, 10)
         self.XARM_JOINT_THRESHOLD = 0.05
         self.OT2_JOINT_THRESHOLD = 0.005
         self.OT2_ASSET_THRESHOLD = 0.25
         self.ASSET_ROTATION_THRESHOLD = 0.1
-        self.assets = {}
+        self.assets = Assets()
         self.asset_subs = {}
         self.asset_poses = {}
         self.initial_asset_poses = {}
         self.target_asset = ""
-        #for asset in self.assets:
-        #    self.asset_subs[f"{asset}"] = self.create_subscription(Float32MultiArray, f"/sim_{asset}/pose", lambda msg: self.asset_cb(msg, f"{asset}"), 10)
-        #    self.initial_asset_poses[f"{asset}"] = np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]) #TODO fix
-        #    self.asset_poses[f"{asset}"] = np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0])
+        self.moving_asset = ""
+        for asset in self.assets:
+           self.asset_subs[f"{asset}"] = self.create_subscription(Float32MultiArray, f"/sim_{asset}/pose", lambda msg: self.asset_cb(msg, f"{asset}"), 10)
+           self.initial_asset_poses[f"{asset}"] = np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]) #TODO fix
+           self.asset_poses[f"{asset}"] = np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0])
+    def target_asset_cb(self, msg):
+        # there is only going to be 1 target and 1 moving asset at a time
+        # msg format: "target_asset, moving_asset"
+        raw = msg.data.split(" ")
+        self.target_asset, self.moving_asset = raw[0], raw[1]
     def asset_cb(self, msg, asset):
         self.asset_poses[f"{asset}"][0], self.asset_cmd_poses[f"{asset}"][1], self.asset_cmd_poses[f"{asset}"][2] = msg.pose.position.x, msg.pose.position.y, msg.pose.position.z
         self.asset_poses[f"{asset}"][3], self.asset_cmd_poses[f"{asset}"][4], self.asset_cmd_poses[f"{asset}"][5], self.asset_cmd_poses[f"{asset}"][6] = msg.pose.orientation.w, msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z
@@ -39,46 +46,85 @@ class SafetyChecker(Node):
         self.xarm_joints = msg.data[:7]
         self.xarm_targets = msg.data[7:]
     def ot2_cb(self, msg):
-        # self.ot2_joints = msg.data[:5]
-        # self.ot2_targets = msg.data[5:]
-        self.ot2_joints = msg.data[:2]
-        self.ot2_targets = msg.data[2:]
+        self.ot2_joints = msg.data[:3]
+        self.ot2_targets = msg.data[3:]
     def check_collision(self):
         # Check collisions of robots and assets
-        return 1
+        return False
     def check_safety(self):
         self.get_logger().info("RUNNING!")
-        self.status.data = 1
         # Implement safety checks here
-        self.status.data = self.check_collision()
-        # Check asset correctness
-        
-
-        if False: #if safety is false
-            self.status.data = -1
+        if self.check_collision():
+            self.status_pub.publish(Int8(data=-1))
+            return
+        # Check if xArm is moving
+        xarm_moving = False
         for i in range(len(self.xarm_joints)):
             # if xArm is moving
             if abs(self.xarm_joints[i] - self.xarm_targets[i]) > self.XARM_JOINT_THRESHOLD:
-                self.status.data = 0
-
+                xarm_moving = True
+                break
+        if xarm_moving:
+            self.status_pub.publish(Int8(data=0))
+            for asset in self.assets:
+                if asset == self.moving_asset:
+                    continue
+                if (np.linalg.norm(self.asset_poses[f"{asset}"][:3] - self.ot2_targets[:2]) > self.OT2_ASSET_THRESHOLD
+                    or np.linalg.norm(self.asset_poses[f"{asset}"][3:] - self.initial_asset_poses[f"{asset}"][3:]) > self.ASSET_ROTATION_THRESHOLD):
+                    self.status_pub.publish(Int8(data=-1))
+                    return
+                # if gripper is closing, check if the asset is not blocked
+                if (self.xarm_joints[7] - self.xarm_targets[7] > self.XARM_JOINT_THRESHOLD):
+                    if asset == self.target_asset:
+                        continue
+                    # if gripper is coming from above, check if asset is blocked from above
+                    if xarm_joint is above target asset: #TODO check gripper position
+                        if ((np.linalg.norm(self.asset_poses[f"{asset}"][:2] - self.asset_poses[f"{self.target_asset}"][:2]) < self.OT2_ASSET_THRESHOLD)
+                            and (self.asset_poses[f"{asset}"][2] > self.asset_poses[f"{self.target_asset}"][2])):
+                            self.status_pub.publish(Int8(data=-1))
+                            return
+                    # if gripper is coming from left, check if asset is blocked from left
+                    if xarm_joint is left of target asset:
+                        if ((np.linalg.norm(self.asset_poses[f"{asset}"][0:3:2] - self.asset_poses[f"{self.target_asset}"][0:2:2]) < self.OT2_ASSET_THRESHOLD)
+                            and (self.asset_poses[f"{asset}"][0] < self.asset_poses[f"{self.target_asset}"][0])):
+                            self.status_pub.publish(Int8(data=-1))
+                            return
+                    # if gripper is coming from right, check if asset is blocked from right
+                    if xarm_joint is right of target asset:
+                        if ((np.linalg.norm(self.asset_poses[f"{asset}"][1:3] - self.asset_poses[f"{self.target_asset}"][1:3]) < self.OT2_ASSET_THRESHOLD)
+                            and (self.asset_poses[f"{asset}"][0] > self.asset_poses[f"{self.target_asset}"][0])):
+                            self.status_pub.publish(Int8(data=-1))
+                            return
+            return
+                        
+        # Check if OT2 is moving
+        ot2_moving = False
         for i in range(len(self.ot2_joints)):
-            # if OT2 is moving
-            if abs(self.ot2_joints[i] - self.ot2_targets[i]) > self.OT2_JOINT_THRESHOLD:
-                self.status.data = 0
-                # for asset in self.assets:
-                # # check if assets are in the correct positions
-                #     if (np.linalg.norm(self.asset_poses[f"{asset}"][:3] - self.ot2_targets[:2]) > self.OT2_ASSET_THRESHOLD
-                #         or np.linalg.norm(self.asset_poses[f"{asset}"][3:] - self.initial_asset_poses[f"{asset}"][3:]) > self.ASSET_ROTATION_THRESHOLD):
-                #         self.status.data = -1
-                # # if pipette is lowering, check if the below asset is not blocked
-                # if (self.ot2_joints[2] - self.ot2_targets[2] > self.OT2_JOINT_THRESHOLD):
-                #     for asset in self.assets:
-                #         if asset == self.target_asset:
-                #             continue
-                #         if ((np.linalg.norm(self.asset_poses[f"{asset}"][:2] - self.asset_poses[f"{self.target_asset}"][:2]) < self.OT2_ASSET_THRESHOLD)
-                #             and (self.asset_poses[f"{asset}"][2] > self.asset_poses[f"{self.target_asset}"][2])):
-                #             self.status.data = -1
-        self.status_pub.publish(self.status)
+            if abs(self.ot2_joints[i] - self.ot2_targets[i] > self.OT2_JOINT_THRESHOLD):
+                ot2_moving = True
+                break
+        if ot2_moving:
+            self.status_pub.publish(Int8(data=0))
+            for asset in self.assets:
+                if asset == self.moving_asset:
+                    continue
+                # check if assets are in the correct positions
+                if (np.linalg.norm(self.asset_poses[f"{asset}"][:3] - self.ot2_targets[:2]) > self.OT2_ASSET_THRESHOLD
+                    or np.linalg.norm(self.asset_poses[f"{asset}"][3:] - self.initial_asset_poses[f"{asset}"][3:]) > self.ASSET_ROTATION_THRESHOLD):
+                    self.status_pub.publish(Int8(data=-1))
+                    return
+                # if pipette is lowering, check if the below asset is not blocked
+                if (self.ot2_joints[2] - self.ot2_targets[2] > self.OT2_JOINT_THRESHOLD):
+                    if asset == self.target_asset:
+                        continue
+                    if ((np.linalg.norm(self.asset_poses[f"{asset}"][:2] - self.asset_poses[f"{self.target_asset}"][:2]) < self.OT2_ASSET_THRESHOLD)
+                        and (self.asset_poses[f"{asset}"][2] > self.asset_poses[f"{self.target_asset}"][2])):
+                        self.status_pub.publish(Int8(data=-1))
+                        return
+            return
+
+        # If robots are stationary and everything is fine, publish 1
+        self.status_pub.publish(Int8(data=1))
 
 def main():
     rclpy.init()

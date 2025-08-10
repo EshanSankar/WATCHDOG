9b205304595130d736bd4e46473d68258d1bb521
EshanSankar <eshansankar12@gmail.com>
2025-08-09 23:33:26 -0400

    a lot of testing

--- digital_to_real.py
+++ digital_to_real.py
@@ -39,7 +39,8 @@ set_camera_view(
 
 # loading assets
 ASSET_PATH = "file:///home/sdl1/isaacsim/sdl1-digital-twin/assets"
-ORCHESTRATOR_PATH = "" #"/home/sdl1/ros2_projects/digital_twin/src/catalyst-OT2-xArm"
+WORKFLOW_PATH = "video_workflow.json"
+
 # loading xarm
 add_reference_to_stage(usd_path=f"{ASSET_PATH}/xarm6_with_gripper.usd", prim_path="/World/xarm6_with_gripper") # robot
 xarm = Articulation(prim_paths_expr="/World/xarm6_with_gripper", name="xarm6") # create an articulation object
@@ -52,11 +53,15 @@ simulation_app.update()
 ot2 = SingleArticulation(prim_path="/World/ot2", name="ot2")
 ot2.initialize()
 simulation_app.update()
-xarm.set_world_poses(positions=np.array([[-0.74, 0.03, 0.0]]) / 1.0, orientations=np.array([[-0.4480736, 0, 0, 0.8939967]]))
-ot2.set_world_pose(position=np.array([[0.0, 0.80, 0.0]]) / 1.0, orientation=np.array([[0.7071068, 0.7071068, 0, 0]]))
+xarm.set_world_poses(positions=np.array([[2, 2, 0.0]]) / 1.0, orientations=np.array([[-0.4480736, 0, 0, 0.8939967]]))
+ot2.set_world_pose(position=np.array([[0.0, 0.0, 0.0]]) / 1.0, orientation=np.array([[0.7071068, 0.7071068, 0, 0]]))
 simulation_app.update()
+xarm.set_joint_positions(positions=np.array([-2.87, -0.2179, -0.7428, -1.13558, 1.4665, -3.1397, 0.3, 0.3]), joint_indices=np.array([0, 1, 2, 3, 4, 5, 10, 11]))
+xarm.set_world_poses(positions=np.array([[-0.525, -0.33, 0.0]]) / 1.0, orientations=np.array([[0.7071068, 0, 0, -0.7071068]]))
+simulation_app.update()
+
 OT2_BASE_HEIGHT = 0.054
-OT2_SLOT_DIMS = {"x": 0.04, "y": 0.025}
+OT2_OFFSETS = {"x": -0.12756, "y": -0.17065}
 OT2_COORDS = {
 	1: [0.0, 0.0], 2: [0.13, 0.0], 3: [0.26, 0.0],
 	4: [0.0, 0.09], 5: [0.13, 0.09], 6: [0.26, 0.09],
@@ -67,18 +72,46 @@ OT2_COORDS = {
 #  more negative = more front,	more negative = more left,    more negative = more down,   more negative = more down
 # [-0.08,0.2] #38 cm			[-0.19, 0.18] #42 cm					[-0.1, 0]					  [-0.1, 0]				
 # pipette dims: 80x130
+OTHER_ASSETS = {"vial_1": [7, -0.046, 0.022, 0.037], "vial_2": [7, 0.046, -0.022, 0.037], "vial_rack_lid": [7, -0.0635, -0.0425, 0.065]}
 
 def LoadAssets(asset_path=ASSET_PATH):
 	assets = {}
-	with open(f"{ORCHESTRATOR_PATH}xarm_workflow.json", "r") as f:
+	with open(f"{WORKFLOW_PATH}", "r") as f:
 		workflow = json.load(f)
 		for asset_type, asset in workflow["global_config"]["labware"].items():
-			add_reference_to_stage(usd_path=f"{asset_path}/{asset_type}/{asset_type}.usd", prim_path=f"/World/{asset_type}")
+			# Below is the default. For now, manually specify paths since asset names aren't yet standardized
+			#add_reference_to_stage(usd_path=f"{asset_path}/{asset_type}/{asset_type}.usda", prim_path=f"/World/{asset_type}")
+			if asset_type == "tip_rack":
+				add_reference_to_stage(usd_path=f"{asset_path}/{asset_type}/tiprack_inst.usda", prim_path=f"/World/{asset_type}")
+			elif asset_type == "reactor":
+				add_reference_to_stage(usd_path=f"{asset_path}/{asset_type}/tiprack_inst.usda", prim_path=f"/World/{asset_type}")
+			elif asset_type == "vial_rack":
+				add_reference_to_stage(usd_path=f"{asset_path}/{asset_type}/vial_rack.usdc", prim_path=f"/World/{asset_type}")
 			assets[asset_type] = [asset["slot"], SingleXFormPrim(prim_path=f"/World/{asset_type}", name=asset_type)]
+			# Will also need to specify default rotation per asset as those are also not standardized
 			assets[asset_type][1].set_world_pose(position=np.array([[
-				OT2_COORDS[asset["slot"]][0] + OT2_SLOT_DIMS["x"],
-				OT2_COORDS[asset["slot"]][1] + OT2_SLOT_DIMS["y"],
-				OT2_BASE_HEIGHT]]) / 1.0) # get_stage_units() -> 1.0
+				OT2_COORDS[asset["slot"]][0] + OT2_OFFSETS["x"],
+				OT2_COORDS[asset["slot"]][1] + OT2_OFFSETS["y"],
+				OT2_BASE_HEIGHT]]) / 1.0, orientation=np.array([1, 0, 0, 0])) # get_stage_units() -> 1.0
+			if asset_type == "tip_rack" or asset_type == "reactor":
+				assets[asset_type][1].set_world_pose(orientation=np.array([0.7071068, 0, 0, -0.7071068]))
+			# Because of non-standard assets, some assets will have their origins at their (0,0) instead of the center of the asset
+			# Since the above calculation is done w/ r.t. the center of the asset, will need to offset
+			if asset_type == "vial_rack":
+				assets[asset_type][1].set_world_pose(position=np.array([[
+				OT2_COORDS[asset["slot"]][0] + OT2_OFFSETS["x"] - 0.0635,
+				OT2_COORDS[asset["slot"]][1] + OT2_OFFSETS["y"] - 0.0425,
+				OT2_BASE_HEIGHT]]))
+	for asset_type, offset in OTHER_ASSETS.items():
+		if asset_type == "vial_1" or asset_type == "vial_2":
+			add_reference_to_stage(usd_path=f"{asset_path}/{asset_type}/vial_20ml_inst.usda", prim_path=f"/World/{asset_type}")
+		if asset_type == "vial_rack_lid":
+			add_reference_to_stage(usd_path=f"{asset_path}/{asset_type}/vial_rack_lid.usda", prim_path=f"/World/{asset_type}")
+		assets[asset_type] = [offset[0], SingleXFormPrim(prim_path=f"/World/{asset_type}", name=asset_type)]
+		assets[asset_type][1].set_world_pose(position=np.array([[
+			OT2_COORDS[offset[0]][0] + OT2_OFFSETS["x"] + offset[1],
+			OT2_COORDS[offset[0]][1] + OT2_OFFSETS["y"] + offset[2],
+			OT2_BASE_HEIGHT + offset[3]]]) / 1.0)
 	return assets
 
 class SimulatedWorld(Node):
@@ -90,12 +123,14 @@ class SimulatedWorld(Node):
 		self.ot2_sub = self.create_subscription(JointState, "/sim_ot2/target_joint_states", self.ot2_cb, 10)
 		self.ot2_joint_targets = np.array([0.0, 0.0, 0.0])
 		self.xarm_sub = self.create_subscription(JointState, "/sim_xarm/target_joint_states", self.xarm_cb, 10)
+		self.xarm_gripper_position_sub = self.create_subscription(Float32, "/orchestrator/gripper_position", self.xarm_gripper_position_cb, 10)
+		self.xarm_gripper_position = 500.0
 		self.ot2_joints = ["PrismaticJointMiddleBar", "PrismaticJointPipetteHolder", "PrismaticJointRightPipette"]
 		self.ot2_joint_indices = np.array([0, 1, 3])
 		self.xarm_joints = ["joint1", "joint2", "joint3", "joint4", "joint5", "joint6", "left_finger_joint", "right_finger_joint"]
 		self.xarm_joint_indices = np.array([0, 1, 2, 3, 4, 5, 10, 11])
-		# gripper joint limits [0, 0.85]; real is [-10 (will use 0), 850]
-		self.xarm_joint_targets = np.array([2.865, -0.0861, -0.4979, 4.3559, 1.3179, 1.021, 0.5, 0.5])
+		# gripper joint limits [0, 0.85]; real is [0, 850]
+		self.xarm_joint_targets = np.array([-2.87, -0.2179, -0.7428, -1.13558, 1.4665, -3.1397, 0.3, 0.3])
 		# /sim_robot/joint_states: [[joint1, ..., jointN], [joint1_goal, ..., jointN_goal]]
 		self.ot2_pub = self.create_publisher(Float32MultiArray, "/sim_ot2/joint_states", 10)
 		self.xarm_pub = self.create_publisher(Float32MultiArray, "/sim_xarm/joint_states", 10)
@@ -118,15 +153,29 @@ class SimulatedWorld(Node):
 		self.asset_pubs = {}
 		self.asset_cmd_poses = {}
 		self.asset_initial_poses = {}
+		self.asset_initial_tracked_poses = {}
+		self.initialized_assets = {}
 		for asset_type, asset in self.assets.items():
+			# will manually skip some stationary assets
+			if asset_type == "tip_rack" or asset_type == "reactor" or asset_type == "vial_rack":
+				continue
 			self.asset_subs[asset_type] = self.create_subscription(PoseStamped, f"/asset_position_{asset_type}", lambda msg, asset_type=asset_type: self.asset_cb(msg, asset_type), 10)
 			self.asset_cmd_poses[asset_type] = np.concatenate((asset[1].get_world_pose()[0], asset[1].get_world_pose()[1]), axis=0)
 			self.asset_initial_poses[asset_type] = self.asset_cmd_poses[asset_type].copy()
 			self.asset_pubs[asset_type] = self.create_publisher(Float32MultiArray, f"/sim_{asset_type}/pose", 10)
+			self.initialized_assets[asset_type] = False
 	def asset_cb(self, msg, asset_type):
-		self.asset_cmd_poses[asset_type][0], self.asset_cmd_poses[asset_type][1], self.asset_cmd_poses[asset_type][2] = msg.pose.position.x, msg.pose.position.y, msg.pose.position.z
-		self.asset_cmd_poses[asset_type][3], self.asset_cmd_poses[asset_type][4], self.asset_cmd_poses[asset_type][5], self.asset_cmd_poses[asset_type][6] = msg.pose.orientation.w, msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z
-		self.asset_cmd_poses[asset_type] = np.subtract(self.asset_cmd_poses[asset_type], self.asset_initial_poses[asset_type])
+		tracked_pose = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z, msg.pose.orientation.w, msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z])
+		if not self.initialized_assets[asset_type]:
+			self.asset_initial_tracked_poses[asset_type] = tracked_pose
+			self.initialized_assets[asset_type] = True
+		else:
+			# Bug with FoundationPose causes it to track the cylindrical vials as constantly rotating along the z-axis
+			# Due to this limitation, will exclude orientation tracking for vials only
+			if asset_type == "vial_1" or asset_type == "vial_2":
+				self.asset_cmd_poses[asset_type][:3] = np.add(self.asset_initial_poses[asset_type][:3], np.subtract(tracked_pose[:3], self.asset_initial_tracked_poses[asset_type][:3]))
+			else:
+				self.asset_cmd_poses[asset_type] = np.add(self.asset_initial_poses[asset_type], np.subtract(tracked_pose, self.asset_initial_tracked_poses[asset_type]))
 	def ot2_cb(self, msg):
 		self.ot2_joint_targets = msg.position
 	def xarm_cb(self, msg):
@@ -135,6 +184,8 @@ class SimulatedWorld(Node):
 			self.xarm_joint_targets = np.concatenate((np.array(msg.position)[:6] if msg.position[6] == 0.0 else np.add(np.array(msg.position)[:6], xarm.get_joint_positions()[0][:6]), self.xarm_joint_targets[6:]))
 		else:
 			self.xarm_joint_targets = np.concatenate((self.xarm_joint_targets[:6], np.array(msg.position)))
+	def xarm_gripper_position_cb(self, msg):
+		self.xarm_gripper_position = msg.data / 1000
 	def safety_cb(self, msg):
 		self.safety = int(msg.data)
 	def xarm_control_cb(self, msg):
@@ -144,7 +195,6 @@ class SimulatedWorld(Node):
 		reset_needed = False
 		while simulation_app.is_running():
 			self.c += 1
-			print(self.c)
 			rclpy.spin_once(self, timeout_sec=0.0)
 			self.ros_world.step(render=True)
 			simulation_app.update()
@@ -156,8 +206,7 @@ class SimulatedWorld(Node):
 						reset_needed = False
 					if self.safety == -1:
 					#if self.c > 400 and self.c < 600: (for testing)
-						# teleoperation #TODO: add gripper control
-						xarm.set_joint_positions(positions=self.xarm_control_joints, joint_indices=self.xarm_joint_indices[:6])
+						xarm.set_joint_positions(positions=np.concatenate((self.xarm_control_joints, np.array([self.xarm_gripper_position, self.xarm_gripper_position]))), joint_indices=self.xarm_joint_indices)
 						for asset_type, _ in self.assets.items():
 							self.assets[asset_type][1].set_world_pose(position=self.asset_cmd_poses[asset_type][:3], orientation=self.asset_cmd_poses[asset_type][3:])
 							self.asset_pubs[asset_type].publish(Float32MultiArray(layout=MultiArrayLayout(dim=self.asset_dim, data_offset=0), data=self.asset_cmd_poses[asset_type]))
@@ -172,8 +221,8 @@ class SimulatedWorld(Node):
 					self.ot2_pub.publish(Float32MultiArray(layout=MultiArrayLayout(dim=self.ot2_dim, data_offset=0), data=np.concatenate((ot2.get_joint_positions()[:3], self.ot2_joint_targets))))
 					self.xarm_pub.publish(Float32MultiArray(layout=MultiArrayLayout(dim=self.xarm_dim, data_offset=0), data=np.concatenate((xarm.get_joint_positions()[0][:6], xarm.get_joint_positions()[0][10:], self.xarm_joint_targets))))
 					self.xarm_gripper_position_pub.publish(Float32MultiArray(layout=MultiArrayLayout(dim=self.xarm_gripper_dim, data_offset=0), data=np.concatenate((xarm_gripper_center.get_world_pose()[0], xarm_gripper_center.get_world_pose()[1]))))
-					# mirror asset positions from real-world
-					for asset_type, _ in self.assets.items():
+					# mirror asset positions from real-world for non-stationary assets
+					for asset_type, _ in self.asset_pubs.items():
 						self.assets[asset_type][1].set_world_pose(position=self.asset_cmd_poses[asset_type][:3], orientation=self.asset_cmd_poses[asset_type][3:])
 						self.asset_pubs[asset_type].publish(Float32MultiArray(layout=MultiArrayLayout(dim=self.asset_dim, data_offset=0), data=self.asset_cmd_poses[asset_type]))
 		self.timeline.stop()
--- safety_checker.py
+++ safety_checker.py
@@ -6,24 +6,41 @@ import numpy as np
 import json
 
 ASSET_PATH = "file:///home/sdl1/isaacsim/sdl1-digital-twin/assets"
-ORCHESTRATOR_PATH = "" #"/home/sdl1/ros2_projects/digital_twin/src/catalyst-OT2-xArm"
+WORKFLOW_PATH = "video_workflow.json"
 OT2_BASE_HEIGHT = 0.054
-OT2_SLOT_DIMS = {"x": 0.04, "y": 0.025}
+OT2_OFFSETS = {"x": -0.12756, "y": -0.17065}
 OT2_COORDS = {
 	1: [0.0, 0.0], 2: [0.13, 0.0], 3: [0.26, 0.0],
 	4: [0.0, 0.09], 5: [0.13, 0.09], 6: [0.26, 0.09],
 	7: [0.0, 0.18], 8: [0.13, 0.18], 9: [0.26, 0.18],
 	10: [0.0, 0.27], 11: [0.13, 0.27], 12: [0.26, 0.27]}
+OTHER_ASSETS = {"vial_1": [7, -0.046, 0.022, 0.037], "vial_2": [7, 0.046, -0.022, 0.037], "vial_rack_lid": [7, -0.0635, -0.0425, 0.065]}
 
 def LoadAssets(asset_path=ASSET_PATH):
 	assets = {}
-	with open(f"{ORCHESTRATOR_PATH}xarm_workflow.json", "r") as f:
+	with open(f"{WORKFLOW_PATH}", "r") as f:
 		workflow = json.load(f)
 		for asset_type, asset in workflow["global_config"]["labware"].items():
 			assets[asset_type] = [asset["slot"], np.array([
-				OT2_COORDS[asset["slot"]][0] + OT2_SLOT_DIMS["x"],
-				OT2_COORDS[asset["slot"]][1] + OT2_SLOT_DIMS["y"],
-				OT2_BASE_HEIGHT])]
+                    OT2_COORDS[asset["slot"]][0] + OT2_OFFSETS["x"],
+                    OT2_COORDS[asset["slot"]][1] + OT2_OFFSETS["y"],
+                    OT2_BASE_HEIGHT,
+                    1, 0, 0, 0])]
+			if asset_type == "tip_rack" or asset_type == "reactor":
+				assets[asset_type][1][3:] = np.array([0.7071068, 0, 0, -0.7071068])
+			# Because of non-standard assets, some assets will have their origins at their (0,0) instead of the center of the asset
+			# Since the above calculation is done w/ r.t. the center of the asset, will need to offset
+			if asset_type == "vial_rack":
+				assets[asset_type][1][:3] = np.array([
+				OT2_COORDS[asset["slot"]][0] + OT2_OFFSETS["x"] - 0.0635,
+				OT2_COORDS[asset["slot"]][1] + OT2_OFFSETS["y"] - 0.0425,
+				OT2_BASE_HEIGHT])
+	for asset_type, offset in OTHER_ASSETS.items():
+		assets[asset_type] = [offset[0], np.array([
+               OT2_COORDS[offset[0]][0] + OT2_OFFSETS["x"] + offset[1],
+               OT2_COORDS[offset[0]][1] + OT2_OFFSETS["y"] + offset[2],
+               OT2_BASE_HEIGHT + offset[3],
+               1, 0, 0, 0])]
 	return assets
 
 class SafetyChecker(Node):
@@ -38,28 +55,28 @@ class SafetyChecker(Node):
         self.xarm_targets = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
         self.xarm_gripper_position = np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0])
         self.ot2_joints = np.array([0.0, 0.0, 0.0])
-        self.ot2_targets = np.array([0.0, 0.0, 0.0])
+        self.ot2_targets = np.array([0.0, 0.0, -0.025])
         self.ot2_pipette_position = np.array([0.0, 0.0, 0.0])
         # STATUS INTS: 0: continue current action, 1: safe, -1: unsafe
         self.status_int = 1
         self.status_pub = self.create_publisher(Int8, "/safety_checker/status_int", 10)
         self.xarm_control = 0
-        #TODO: watch for a topic that indicates whether teleop or not
-        self.xarm_control_sub = self.create_subscription(TYPE, "TOPIC", self.xarm_control_cb, 10)
+        self.xarm_control_sub = self.create_subscription(Int8, "/orchestrator/resolution", self.xarm_control_cb, 10)
         self.target_asset_ot2_sub = self.create_subscription(String, "/sim_ot2/target_asset", self.target_asset_ot2_cb, 10)
         self.target_asset_xarm_sub = self.create_subscription(String, "/sim_xarm/target_asset", self.target_asset_xarm_cb, 10)
         self.XARM_JOINT_THRESHOLD = 0.05 # check if xArm is moving
         self.XARM_GRIPPER_THRESHOLD = 0.1 # xArm grasping position
         self.XARM_CARRY_THRESHOLD = 0.1 # xArm carrying
-        self.OT2_JOINT_THRESHOLD = 0.005 # check if OT2 is moving
-        self.OT2_ASSET_POSITION_THRESHOLD = 0.25 # check assets are in correct spots relative to pipette
-        self.ASSET_ROTATION_THRESHOLD = 0.1 # check if assets are oriented properlys
+        self.OT2_JOINT_THRESHOLD = 0.5 # check if OT2 is moving
+        self.OT2_ASSET_POSITION_THRESHOLD = 0.5 # check assets are in correct spots relative to pipette
+        self.ASSET_ROTATION_THRESHOLD = 999 # check if assets are oriented properly
         self.assets = LoadAssets()
         self.asset_subs = {}
         self.asset_poses = {}
         self.initial_asset_poses = {}
         self.target_asset_xarm = ""
         self.target_asset_ot2 = ""
+        self.xarm_carrying = False
         for asset_type, _ in self.assets.items():
            self.asset_subs[asset_type] = self.create_subscription(Float32MultiArray, f"/sim_{asset_type}/pose",
                                                                   lambda msg, asset_type=asset_type: self.asset_cb(msg, asset_type), 10)
@@ -72,8 +89,7 @@ class SafetyChecker(Node):
     def target_asset_xarm_cb(self, msg):
         self.target_asset_xarm = msg.data
     def asset_cb(self, msg, asset_type):
-        self.asset_poses[asset_type] = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z,
-                                                 msg.pose.orientation.w, msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z])
+        self.asset_poses[asset_type] = msg.data
     def xarm_cb(self, msg):
         self.xarm_joints = msg.data[:8]
         self.xarm_targets = msg.data[8:]
@@ -85,8 +101,7 @@ class SafetyChecker(Node):
     def ot2_pipette_cb(self, msg):
         self.ot2_pipette_position = msg.data
     def xarm_control_cb(self, msg):
-        #TODO
-        self.xarm_control = 1 if msg.data == ____ else 0
+        self.xarm_control = 1 if msg.data == 1 else 0
     # Primary functions
     def xarm_collision(self):
         # Check if xArm collides with the OT2, table surface, etc.
@@ -101,7 +116,7 @@ class SafetyChecker(Node):
                 return
             self.xarm_control = 0
             self.status_int = 1
-
+        self.get_logger().info("Checked xArm control")
         # 1. Check if xArm arm is moving (gripper doesn't count as moving)
         xarm_moving = False
         for i in range(len(self.xarm_joints) - 2): # Exclude gripper joints
@@ -117,20 +132,21 @@ class SafetyChecker(Node):
                 return
             for asset_type, _ in self.assets.items():
                 if asset_type == self.target_asset_xarm: # If object is being carried, ensure gripper hasn't dropped it
-                    if np.linalg.norm(self.xarm_gripper_position[:3] - self.asset_poses[self.target_asset_xarm][:3]) > self.XARM_CARRY_THRESHOLD:
-                        self.status_pub.publish(Int8(data=-1))
-                        self.status_int = -1
-                        self.xarm_control = 1
-                        return
+                    if self.xarm_carrying:
+                        if np.linalg.norm(self.xarm_gripper_position[:3] - self.asset_poses[self.target_asset_xarm][:3]) > self.XARM_CARRY_THRESHOLD:
+                            self.status_pub.publish(Int8(data=-1))
+                            self.status_int = -1
+                            self.xarm_control = 1
+                            return
                     continue
                 # Check that remaining objects are in proper positions
-                if (np.linalg.norm(self.asset_poses[asset_type[:3]] - self.initial_asset_poses[asset_type][:3]) > self.OT2_ASSET_POSITION_THRESHOLD) or \
+                if (np.linalg.norm(self.asset_poses[asset_type][:3] - self.initial_asset_poses[asset_type][:3]) > self.OT2_ASSET_POSITION_THRESHOLD) or \
                     (np.linalg.norm(self.asset_poses[asset_type][3:] - self.initial_asset_poses[asset_type][3:]) > self.ASSET_ROTATION_THRESHOLD):
                     self.status_pub.publish(Int8(data=-1))
                     self.status_int = -1
                     self.xarm_control = 1
                     return
-                
+        self.get_logger().info("Checked xArm motion")
         # 2. Check if xArm is about to grip something
         if self.target_asset_xarm != "":
             # 2.1: Check if gripper is closing; can use [7] or [8]
@@ -171,18 +187,21 @@ class SafetyChecker(Node):
                             self.status_pub.publish(Int8(data=-1))
                             self.status_int = -1
                             self.xarm_control = 1
-                            return                
+                            return
+                self.xarm_carrying = True                
                 return
              # 2.2: Check if gripper is releasing
             elif self.xarm_targets[7] > self.xarm_joints[7]:
                 # Update target object's initial pose
                 self.initial_asset_poses[self.target_asset_xarm] = self.asset_poses[self.target_asset_xarm]
-        
+                self.xarm_carrying = False
+        self.get_logger().info("Checked xArm gripper")
         # 3. Check if OT2 is moving
         ot2_moving = False
         for i in range(len(self.ot2_joints)):
             if abs(self.ot2_joints[i] - self.ot2_targets[i]) > self.OT2_JOINT_THRESHOLD:
                 ot2_moving = True
+                self.get_logger().info("OT2 is moving")
                 break
         if ot2_moving:
             self.status_pub.publish(Int8(data=0))
@@ -208,10 +227,12 @@ class SafetyChecker(Node):
                         self.xarm_control = 1
                         return
             return
+        self.get_logger().info("Checked OT2 movement")
 
         # 4. If robots are stationary and everything is fine, publish 1
         self.status_pub.publish(Int8(data=1))
         self.status_int = 1
+        self.get_logger().info("All good")
 
 def main():
     rclpy.init()
